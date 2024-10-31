5b10862c2660a1e9ad1758b4f209d1ab2e517bcc
Cheonilyeong <2071178@hansung.ac.kr>
2024-11-01 00:11:20 +0900

    feat: 각 feature 모듈 compose 의존성 추가(#10)

--- feature/category/build.gradle.kts
+++ feature/category/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -35,6 +36,10 @@ android {
 dependencies {
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.ui.tooling.preview)
+    debugImplementation(libs.androidx.ui.tooling)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
