5b3aa7046c7503a6450b9f86d4b8f064731e149c
915dbfl <dbfl00915@naver.com>
2024-11-05 16:15:15 +0900

    feat: firebase 빌드 로직 별도로 구성(#12)

---
+++ build-logic/src/main/java/kr/boostcamp_2024/course/build_logic/Firebase.kt
@@ -0,0 +1,19 @@
+package kr.boostcamp_2024.course.build_logic
+
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+internal fun Project.configureFirebase() {
+    with(pluginManager) {
+        apply("com.google.gms.google-services")
+    }
+
+    dependencies {
+        val libs = project.extensions.libs
+        val bom = libs.findLibrary("firebase-bom").get()
+        add("implementation", platform(bom))
+        add("implementation", libs.findLibrary("firebase.auth").get())
+        add("implementation", libs.findLibrary("firebase.firestore").get())
+    }
+
+}
