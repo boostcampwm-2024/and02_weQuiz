f6f5b69b317636bfa536cba1a11937de8427d152
915dbfl <dbfl00915@naver.com>
2024-11-05 14:23:20 +0900

    feat: 필요한 의존성 추가(gradle plugin, desugarJdkLibs)(#12)

--- build-logic/build.gradle.kts
+++ build-logic/build.gradle.kts
@@ -6,4 +6,5 @@ dependencies {
     implementation(libs.android.gradlePlugin)
     implementation(libs.android.desugarJdkLibs)
     implementation(libs.kotlin.gradlePlugin)
+    compileOnly(libs.compose.compiler.gradle.plugin)
 }
