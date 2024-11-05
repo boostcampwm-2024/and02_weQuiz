fff1f3d24c31b6ae2031dfbf9bf8ca159a2e1232
mk43313@gmail.com <mk43313@gmail.com>
2024-11-06 04:56:07 +0900

    feat: GroupItem 생성(#24)

--- feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/GroupScreen.kt
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/GroupScreen.kt
@@ -1,8 +1,27 @@
 package kr.boostcamp_2024.course.study.presentation
 
+import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.study.R
+import kr.boostcamp_2024.course.study.component.CustomPropertyTab
 
 @Composable
 fun GroupScreen() {
-
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 16.dp, end = 16.dp, top = 8.dp)
+    ) {
+        CustomPropertyTab(
+            onClicked = { Log.d("detail", "그룹 클릭됨") },
+            icon = painterResource(R.drawable.baseline_add_circle_outline_24),
+            title = "그룹원"
+        )
+    }
 }
