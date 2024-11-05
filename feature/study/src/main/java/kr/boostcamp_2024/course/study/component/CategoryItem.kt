0e8bd1e1c1c11b7ecd550e6e71ebfe35a845c181
mk43313@gmail.com <mk43313@gmail.com>
2024-11-06 04:12:17 +0900

    feat: CategoryItem 생성(#22)

--- feature/study/src/main/java/kr/boostcamp_2024/course/study/component/CategoryItem.kt
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/component/CategoryItem.kt
@@ -3,19 +3,24 @@ package kr.boostcamp_2024.course.study.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -35,12 +40,14 @@ fun CategoryItem(
             .clickable(onClick = onClicked)
     ) {
         Box(modifier = Modifier.size(120.dp)) {
-            CircleImg(
+            CustomRoundImg(
                 categoryImg, modifier = Modifier
                     .fillMaxSize()
                     .clip(RoundedCornerShape(16))
             )
-            Box(modifier = Modifier.padding(top = 8.dp, end = 8.dp).align(Alignment.TopEnd)) {
+            Box(modifier = Modifier
+                .padding(top = 8.dp, end = 8.dp)
+                .align(Alignment.TopEnd)) {
                 Box(
                     modifier = Modifier
                         .size(16.dp)
@@ -51,5 +58,46 @@ fun CategoryItem(
                 }
             }
         }
+        DetailStudyDescription(title, content, profileImg, author)
+    }
+}
+
+@Composable
+fun DetailStudyDescription(
+    title: String,
+    content: String,
+    profileImg: String? = null,
+    author: String
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxHeight()
+            .padding(start = 16.dp)
+    ) {
+        Text(modifier = Modifier.padding(bottom = 4.dp), text = title, style = MaterialTheme.typography.titleLarge)
+        Text(
+            modifier = Modifier.padding(bottom = 12.dp),
+            text = content,
+            style = MaterialTheme.typography.bodyMedium,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        AuthorComponent(profileImg, author)
+    }
+}
+
+@Composable
+fun AuthorComponent(
+    profileImg: String? = null,
+    author: String
+) {
+    Row(modifier = Modifier.padding(bottom = 8.dp), verticalAlignment = Alignment.CenterVertically) {
+        CustomRoundImg(
+            profileImg, modifier = Modifier
+                .clip(CircleShape)
+                .size(24.dp)
+        )
+        Text(modifier = Modifier.padding(start = 4.dp), text = author, style = MaterialTheme.typography.bodySmall)
     }
 }
