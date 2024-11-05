d05e44c50c88981fa98081ce8afa507fa61b0f4b
mk43313@gmail.com <mk43313@gmail.com>
2024-11-06 03:47:49 +0900

    feat: DetailStudyTopBar 생성(#22)

--- feature/study/src/main/java/kr/boostcamp_2024/course/study/component/DetailStudyTopBar.kt
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/component/DetailStudyTopBar.kt
@@ -4,17 +4,24 @@ import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.boostcamp_2024.course.study.R
@@ -23,7 +30,9 @@ import kr.boostcamp_2024.course.study.R
 @Composable
 fun DetailStudyTopBar() {
     Box(
-        modifier =  Modifier.fillMaxWidth().height(350.dp)
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(350.dp)
     ) {
         Image(
             painter = painterResource(R.drawable.waterfall),
@@ -50,6 +59,23 @@ fun DetailStudyTopBar() {
                 icon = painterResource(R.drawable.baseline_settings_24),
                 description = "설정 아이콘 버튼"
             )
+        }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .align(Alignment.BottomStart)
+        ) {
+            Text(text = "OS 스터디", style = MaterialTheme.typography.displayMedium, fontWeight = FontWeight.SemiBold)
+            Text(text = "os 와압!!!", style = MaterialTheme.typography.bodySmall)
+            Button(onClick = {}, shape = RoundedCornerShape(8.dp)) {
+                Icon(
+                    modifier = Modifier.size(18.dp),
+                    painter = painterResource(R.drawable.baseline_account_circle_24),
+                    contentDescription = "그룹원 수 버튼"
+                )
+                Text(modifier = Modifier.padding(start = 8.dp), text = "10명 / 50명", style = MaterialTheme.typography.labelLarge)
+            }
         }
     }
 }
