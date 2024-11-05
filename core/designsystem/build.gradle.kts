b7085f255ca748d0561342269048507abd2ccf19
915dbfl <dbfl00915@naver.com>
2024-11-05 16:09:01 +0900

    feat: core/designsystem에 convention 빌드 로직 적용(#12)

--- core/designsystem/build.gradle.kts
+++ core/designsystem/build.gradle.kts
@@ -1,53 +1,16 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
-
+    id("convention.android.library")
+    id("convention.android.compose")
 }
 
 android {
     namespace = "kr.boostcamp_2024.course.designsystem"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-    buildFeatures{
-        compose = true
-    }
-
 }
 
 dependencies {
-    implementation(libs.material3)
-    implementation(libs.ui)
-    implementation(platform(libs.androidx.compose.bom))
-
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
