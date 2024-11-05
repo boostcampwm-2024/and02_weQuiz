bde4d28687f0dedb3d4ffdc20ddeb836ece10d39
915dbfl <dbfl00915@naver.com>
2024-11-05 14:59:15 +0900

    feat: 필요한 precompiled script plugin 구성(#12)

---
+++ build-logic/src/main/java/convention.android.feature.gradle.kts
@@ -0,0 +1,22 @@
+import kr.boostcamp_2024.course.build_logic.androidExtension
+import kr.boostcamp_2024.course.build_logic.libs
+
+plugins {
+    id("convention.android.library")
+    id("convention.android.compose")
+    id("convention.android.hilt")
+}
+
+androidExtension.apply {
+    pluginManager.apply("org.jetbrains.kotlin.plugin.serialization")
+
+    dependencies {
+        val libs = project.extensions.libs
+        add("implementation", project(":core:domain"))
+        add("implementation", project(":core:designsystem"))
+        add("implementation", libs.findLibrary("androidx.navigation.compose").get())
+        add("implementation", libs.findLibrary("kotlinx.serialization.json").get())
+    }
+}
+
+
---
+++ build-logic/src/main/java/convention.android.hilt.gradle.kts
@@ -0,0 +1,3 @@
+import kr.boostcamp_2024.course.build_logic.configureHiltAndroid
+
+configureHiltAndroid()
