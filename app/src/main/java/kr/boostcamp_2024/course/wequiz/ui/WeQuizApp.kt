477fc9d9e406bfd5ef076b575c0eb70e97271b90
Cheonilyeong <2071178@hansung.ac.kr>
2024-11-06 20:52:17 +0900

    feat: 퀴즈 생성 화면 titleTextField UI 구현(#30)

--- app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/WeQuizApp.kt
+++ app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/WeQuizApp.kt
@@ -1,17 +1,16 @@
 package kr.boostcamp_2024.course.wequiz.ui
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
+import kr.boostcamp_2024.course.quiz.presentation.quiz.CreateQuizScreen
 
 @Composable
 fun WeQuizApp() {
     WeQuizTheme {
         Scaffold { innerPadding ->
-            WeQuizNavHost(
-                modifier = Modifier.padding(innerPadding),
+            CreateQuizScreen(
+                onNavigationButtonClick = {},
             )
         }
     }
--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/quiz/CreateQuizScreen.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/quiz/CreateQuizScreen.kt
@@ -11,13 +11,19 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.outlined.Cancel
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -64,10 +70,37 @@ fun CreateQuizScreen(
                 ProfileCircleImage(modifier = Modifier.size(120.dp))
                 ChatBubbleLeft(text = "추가할 퀴즈에 대한\n정보를 입력해주세요!")
             }
+
+            // QuizTtitle
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                var quizTitle by remember { mutableStateOf("") }
+
+                TextField(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = quizTitle,
+                    onValueChange = { quizTitle = it },
+                    label = {
+                        Text(text = "제목")
+                    },
+                    placeholder = {
+                        Text(text = "퀴즈 제목을 입력하세요.")
+                    },
+                    trailingIcon = {
+                        IconButton(onClick = { quizTitle = "" }) {
+                            Icon(
+                                imageVector = Icons.Outlined.Cancel,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                )
+            }
         }
     }
 }
-
 
 @Preview(showBackground = true)
 @Composable
