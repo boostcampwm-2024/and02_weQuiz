c066f84a7f709110c61c98f13541dbc3b6a983a0
Cheonilyeong <2071178@hansung.ac.kr>
2024-11-06 18:18:25 +0900

    feat: Character Guide UI 구현(#30)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/quiz/CreateQuizScreen.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/quiz/CreateQuizScreen.kt
@@ -1,5 +1,14 @@
 package kr.boostcamp_2024.course.quiz.presentation.quiz
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -9,7 +18,12 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.quiz.presentation.component.ChatBubbleLeft
+import kr.boostcamp_2024.course.quiz.presentation.component.ProfileCircleImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -33,7 +47,24 @@ fun CreateQuizScreen(
             )
         }
     ) { innerPadding ->
-
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        ) {
+            // Character Guide
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp, vertical = 10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                ProfileCircleImage(modifier = Modifier.size(120.dp))
+                ChatBubbleLeft(text = "추가할 퀴즈에 대한\n정보를 입력해주세요!")
+            }
+        }
     }
 }
 
