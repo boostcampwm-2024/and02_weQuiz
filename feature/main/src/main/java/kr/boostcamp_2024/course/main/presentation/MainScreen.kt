da3110d872b1c55145e28c38cbc8fc6918384fdc
Cheonilyeong <2071178@hansung.ac.kr>
2024-11-06 16:13:54 +0900

    feat: 메인화면 유저 프로필, 이름 UI 구현(#27)

--- feature/main/src/main/java/kr/boostcamp_2024/course/main/presentation/MainScreen.kt
+++ feature/main/src/main/java/kr/boostcamp_2024/course/main/presentation/MainScreen.kt
@@ -1,6 +1,14 @@
 package kr.boostcamp_2024.course.main.presentation
 
+import android.content.res.Configuration
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.outlined.Notifications
@@ -9,12 +17,23 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalLayoutDirection
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.main.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -46,9 +65,54 @@ fun MainScreen(
         }
     ) { innerPadding ->
 
+        Column(
+            modifier = Modifier
+                .padding(
+                    PaddingValues(  // Ignore Top Padding
+                        start = innerPadding.calculateLeftPadding(LocalLayoutDirection.current),
+                        end = innerPadding.calculateRightPadding(LocalLayoutDirection.current),
+                        bottom = innerPadding.calculateBottomPadding()
+                    )
+                )
+        ) {
+            UserContent()
+        }
+    }
+}
+
+@Composable
+fun UserContent() {
+    val configuration = LocalConfiguration.current
+
+    val imageHeight = when (configuration.orientation) {
+        Configuration.ORIENTATION_LANDSCAPE -> 150.dp
+        else -> 300.dp
     }
 
+    Box {
+        Image(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(imageHeight),
+            painter = painterResource(id = R.drawable.sample),
+            contentScale = ContentScale.Crop,
+            contentDescription = null
+        )
+
+        Text(
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomStart),
+            text = "홍길동",
+            style = MaterialTheme.typography.displayMedium,
+            fontWeight = FontWeight.SemiBold,
+            color = MaterialTheme.colorScheme.onPrimary,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
