81ed2c8aeb412ad4ee7d988d20b49fdadd5e8f01
pengcon <vpdzjs@gmail.com>
2024-11-06 17:17:31 +0900

    refactor: weQuizApp Scaffold 제거(#28)

--- app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/WeQuizApp.kt
+++ app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/WeQuizApp.kt
@@ -1,18 +1,11 @@
 package kr.boostcamp_2024.course.wequiz.ui
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 
 @Composable
 fun WeQuizApp() {
     WeQuizTheme {
-        Scaffold { innerPadding ->
-            WeQuizNavHost(
-                modifier = Modifier.padding(innerPadding),
-            )
-        }
+        WeQuizNavHost()
     }
 }
