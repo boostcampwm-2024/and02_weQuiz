97ec0177215990cd3d49259f187b90227b08c93a
Cheonilyeong <2071178@hansung.ac.kr>
2024-11-06 16:20:20 +0900

    feat: 메인화면 스터디 리스트 UI 구현(#27)

--- feature/main/src/main/java/kr/boostcamp_2024/course/main/presentation/MainScreen.kt
+++ feature/main/src/main/java/kr/boostcamp_2024/course/main/presentation/MainScreen.kt
@@ -2,28 +2,44 @@ package kr.boostcamp_2024.course.main.presentation
 
 import android.content.res.Configuration
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.outlined.Star
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.PrimaryTabRow
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -76,6 +92,7 @@ fun MainScreen(
                 )
         ) {
             UserContent()
+            UserStudyContent(onStudyClick = onStudyClick)
         }
     }
 }
@@ -113,6 +130,182 @@ fun UserContent() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserStudyContent(
+    onStudyClick: () -> Unit,
+) {
+
+    var state by remember { mutableStateOf(0) }
+    val titles = listOf("참여 스터디", "보관함")
+
+    PrimaryTabRow(selectedTabIndex = state) {
+        titles.forEachIndexed { index, title ->
+            Tab(
+                modifier = Modifier.padding(top = 8.dp),
+                selected = state == index,
+                onClick = { state = index },
+                text = {
+                    Text(
+                        text = title,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                }
+            )
+        }
+    }
+
+    when (state) {
+        0 -> {
+            StudyTab(onStudyClick = onStudyClick)
+        }
+
+        1 -> {
+            // TODO: 보관함
+        }
+    }
+}
+
+@Composable
+fun StudyTab(
+    onStudyClick: () -> Unit,
+) {
+    LazyColumn {
+        item {
+            StudyItem(
+                studyUrl = "",
+                studyTitle = "안드로이드 개발자",
+                studyDescription = "안드로이드 개발자를 위한 스터디입니다.",
+                studyMember = 3,
+                onStudyClick = onStudyClick
+            )
+        }
+
+        item {
+            StudyItem(
+                studyUrl = "",
+                studyTitle = "OS 스터디",
+                studyDescription = "os 와압!",
+                studyMember = 10,
+                onStudyClick = onStudyClick
+            )
+        }
+
+        item {
+            StudyItem(
+                studyUrl = "",
+                studyTitle = "웹개발자",
+                studyDescription = "웹개발자를 위한 스터디입니다.",
+                studyMember = 5,
+                onStudyClick = onStudyClick
+            )
+        }
+
+        item {
+            StudyItem(
+                studyUrl = "",
+                studyTitle = "네트워크 스터디",
+                studyDescription = "",
+                studyMember = 5,
+                onStudyClick = onStudyClick
+            )
+        }
+
+        item {
+            StudyItem(
+                studyUrl = "",
+                studyTitle = "일본어 스터디",
+                studyDescription = "곤니찌와~",
+                studyMember = 6,
+                onStudyClick = onStudyClick
+            )
+        }
+
+        item {
+            StudyItem(
+                studyUrl = "",
+                studyTitle = "요리왕 비룡",
+                studyDescription = "비 내리는 날엔 난 항상 널 그리워 해.",
+                studyMember = 6,
+                onStudyClick = onStudyClick
+            )
+        }
+    }
+}
+
+@Composable
+fun StudyItem(
+    studyUrl: String,
+    studyTitle: String,
+    studyDescription: String,
+    studyMember: Int,
+    onStudyClick: () -> Unit,
+) {
+
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Icon( // TODO studyUrl
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(MaterialTheme.shapes.large)
+                    .background(MaterialTheme.colorScheme.outlineVariant),
+                imageVector = Icons.Outlined.Star, contentDescription = null
+            )
+
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .clickable(onClick = onStudyClick)
+            ) {
+                Text(
+                    text = studyTitle,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+
+                if (studyDescription.isNotEmpty()) {
+                    Text(
+                        text = studyDescription,
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = MaterialTheme.colorScheme.onSurfaceVariant
+                    )
+                }
+
+                Text(
+                    text = "인원: ${studyMember}명",
+                    style = MaterialTheme.typography.bodyMedium,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }
+
+            IconButton(
+                modifier = Modifier.size(24.dp),
+                onClick = { /* TODO : ex) 스터디 나가기 */ }
+            ) {
+                Icon(imageVector = Icons.Default.MoreVert, contentDescription = null)
+            }
+        }
+
+        HorizontalDivider(thickness = 1.dp)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StudyItemPreview() {
+    StudyItem(
+        studyUrl = "",
+        studyTitle = "안드로이드 개발자",
+        studyDescription = "안드로이드 개발자를 위한 스터디입니다.",
+        studyMember = 3,
+        onStudyClick = {}
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
