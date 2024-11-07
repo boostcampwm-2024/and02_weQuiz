a1ae93c19f3c627ae9e6110241d575a6a63388e7
pengcon <vpdzjs@gmail.com>
2024-11-07 13:12:50 +0900

    feat: CreateStudyTopAppBar 생성(#28)

---
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/component/CreateStudyTopAppBar.kt
@@ -0,0 +1,37 @@
+package kr.boostcamp_2024.course.study.component
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+
+@ExperimentalMaterial3Api
+@Composable
+fun CreateStudyTopAppBar(
+    onNavigationButtonClick: () -> Unit,
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = "스터디 생성", style = MaterialTheme.typography.titleLarge
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = onNavigationButtonClick) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    contentDescription = "Back",
+                )
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = MaterialTheme.colorScheme.surface
+        ),
+    )
+}
