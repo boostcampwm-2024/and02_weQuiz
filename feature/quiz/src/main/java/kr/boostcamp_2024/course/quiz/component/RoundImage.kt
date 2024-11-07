b81ef7cd002358c34e6bf400dbcb53a6f5663824
moyeongmin <qa98589858@gmail.com>
2024-11-07 22:02:20 +0900

    feat: 원형 프로필 이미지를 위한 RoundImage 컴포저블 및 시스템 프로필 이미지 추가(#31)

---
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/RoundImage.kt
@@ -0,0 +1,30 @@
+package kr.boostcamp_2024.course.quiz.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.quiz.R
+
+@Composable
+fun RoundImage(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .size(120.dp)
+            .clip(CircleShape)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.quiz_system_profile),
+            contentDescription = "System Profile Image",
+            modifier = Modifier.align(Alignment.Center),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
