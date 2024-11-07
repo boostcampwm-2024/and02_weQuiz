d864f4648ff0db6f51f57a44fdb73420ae46eebb
915dbfl <dbfl00915@naver.com>
2024-11-07 15:17:00 +0900

    feat: mainActivity에 entrypoint 설정(#26)

--- app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/MainActivity.kt
+++ app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.EntryPoint
+import dagger.hilt.android.AndroidEntryPoint
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
