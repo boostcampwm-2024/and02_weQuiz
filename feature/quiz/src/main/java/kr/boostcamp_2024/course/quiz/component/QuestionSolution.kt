453d923207f3b6df0070829756a77755d97ff64c
pengcon <vpdzjs@gmail.com>
2024-11-07 15:47:29 +0900

    feat: 문제 상세 설명의 해설 화면 생성(#39)

---
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/QuestionSolution.kt
@@ -0,0 +1,57 @@
+package kr.boostcamp_2024.course.quiz.component
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.quiz.R
+
+@Composable
+fun QuestionSolution(solution: String) {
+    Column(
+        modifier = Modifier.padding(
+            start = 16.dp, end = 16.dp, top = 0.dp, bottom = 10.dp
+        )
+    ) {
+        Text(
+            text = "해설", modifier = Modifier, style = MaterialTheme.typography.bodyMedium
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(modifier = Modifier) {
+            ChatBubble(
+                solution,
+                modifier = Modifier.weight(1f)
+            )
+
+            Box(
+                modifier = Modifier
+                    .padding(start = 10.dp)
+                    .align(Alignment.CenterVertically)
+                    .size(120.dp)
+                    .clip(CircleShape)
+
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.create_study_character),
+                    contentDescription = "Profile",
+                    modifier = Modifier,
+                    contentScale = ContentScale.Crop
+                )
+            }
+        }
+    }
+}
