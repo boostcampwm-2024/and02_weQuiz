58ec79974861be9a4dbbbd148740f0a9b3c8d980
915dbfl <dbfl00915@naver.com>
2024-11-07 10:34:44 +0900

    feat: CreateQuestionViewModel 추가(#26)

---
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/CreateQuestionViewModel.kt
@@ -0,0 +1,10 @@
+package kr.boostcamp_2024.course.quiz
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CreateQuestionViewModel @Inject constructor(): ViewModel() {
+
+}
