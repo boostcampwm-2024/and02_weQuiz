5b854672847ab134bf823501f0b80ad28dac0c9d
Cheonilyeong <2071178@hansung.ac.kr>
2024-11-07 11:10:50 +0900

    feat: 퀴즈 생성 화면 풀이시간 slider UI 구현(#30)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/quiz/CreateQuizScreen.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/quiz/CreateQuizScreen.kt
@@ -1,9 +1,11 @@
 package kr.boostcamp_2024.course.quiz.presentation.quiz
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -21,18 +23,23 @@ import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kr.boostcamp_2024.course.quiz.presentation.component.ChatBubbleLeft
@@ -174,6 +181,33 @@ fun CreateQuizScreen(
                         )
                     }
                 }
+
+                Text(text = "풀이 시간 (단위: 분)")
+
+                var sliderPosition by remember { mutableFloatStateOf(0f) }
+
+                Slider(
+                    modifier = Modifier.fillMaxWidth(),
+                    value = sliderPosition,
+                    onValueChange = { sliderPosition = it },
+                    steps = 8,
+                    valueRange = 10f..100f,
+                    thumb = {
+                        Box(
+                            modifier = Modifier
+                                .defaultMinSize(minWidth = 40.dp)
+                                .clip(MaterialTheme.shapes.large)
+                                .background(MaterialTheme.colorScheme.primary),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = (sliderPosition.toInt()).toString(),
+                                color = MaterialTheme.colorScheme.onPrimary,
+                                textAlign = TextAlign.Center
+                            )
+                        }
+                    }
+                )
             }
         }
     }
