16b6818e30a8764aba44193562ecaa44f1f267d3
915dbfl <dbfl00915@naver.com>
2024-11-10 00:48:08 +0900

    refactor: password textField에 visualTransformation, keyboardOptions 적용 및 weQuizTextField 코드 개선(#38)

--- feature/login/src/main/java/kr/boostcamp_2024/course/login/presentation/LoginScreen.kt
+++ feature/login/src/main/java/kr/boostcamp_2024/course/login/presentation/LoginScreen.kt
@@ -9,16 +9,26 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -80,6 +90,9 @@ fun LoginGuideImageAndText() {
 
 @Composable
 fun LoginContent() {
+    var showPassword by remember { mutableStateOf(false) }
+    var password by remember { mutableStateOf("") }
+
     Column(
         modifier = Modifier.padding(top = 10.dp),
         verticalArrangement = Arrangement.spacedBy(15.dp),
@@ -93,9 +106,29 @@ fun LoginContent() {
 
         WeQuizTextField(
             label = stringResource(R.string.txt_login_password_label),
-            text = "",
-            onTextChanged = {},
-            placeholder = stringResource(R.string.txt_login_password_placeholder)
+            text = password,
+            onTextChanged = { password = it },
+            placeholder = stringResource(R.string.txt_login_password_placeholder),
+            visualTransformation = if (showPassword) {
+                VisualTransformation.None
+            } else {
+                PasswordVisualTransformation()
+            },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            trailingIcon = {
+                IconButton(
+                    onClick = { showPassword = !showPassword }
+                ) {
+                    Icon(
+                        painter = if (showPassword) {
+                            painterResource(R.drawable.baseline_visibility_24)
+                        } else {
+                            painterResource(R.drawable.baseline_visibility_off_24)
+                        },
+                        contentDescription = null
+                    )
+                }
+            }
         )
     }
 }
