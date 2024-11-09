6beeef864eeb64489e64a5a90bbe0d9dd862df38
mk43313@gmail.com <mk43313@gmail.com>
2024-11-10 04:29:22 +0900

    refactor: 이메일 형식이 아니거나 미작성시 초대 비활성화(#50)

--- feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/CreateGroupScreen.kt
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/CreateGroupScreen.kt
@@ -1,12 +1,12 @@
 package kr.boostcamp_2024.course.study.presentation
 
+import android.util.Patterns
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizBaseDialog
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizTextField
@@ -15,21 +15,23 @@ import kr.boostcamp_2024.course.study.R
 @Composable
 fun CreateGroupScreen(onDismissButtonClick: () -> Unit, onConfirmButtonClick: () -> Unit) {
     var email by remember { mutableStateOf("") }
+    val isEmailValid = remember(email) { Patterns.EMAIL_ADDRESS.matcher(email).matches() }
     WeQuizBaseDialog(
-        title = stringResource(R.string.dialog_create_group_title),
+        title = "그룹원의 이메일을 입력해주세요!\n초대 메일이 갑니다!",
         dialogImage = painterResource(id = R.drawable.waterfall),
-        confirmTitle = stringResource(R.string.btn_dialog_create_group_confirm),
-        dismissTitle = stringResource(R.string.btn_dialog_create_group_dismiss),
-        onConfirm = onConfirmButtonClick,
+        confirmTitle = "초대",
+        dismissTitle = "취소",
+        onConfirm = { if (isEmailValid) onConfirmButtonClick() },
         onDismissRequest = onDismissButtonClick,
         content = {
             WeQuizTextField(
-                label = stringResource(R.string.textfield_label_create_group),
+                label = "그룹원 이메일",
                 text = email,
                 onTextChanged = { email = it },
-                placeholder = stringResource(R.string.textfield_placeholder_create_group)
+                placeholder = "그룹원 이메일을 입력주세요."
             )
         },
+        confirmButtonEnabled = isEmailValid
     )
 }
 
