acd37a2b80610156dcb5b598e14f127d008ccc1c
mk43313@gmail.com <mk43313@gmail.com>
2024-11-09 22:01:25 +0900

    refactor: bottom navigation navHost 대신 when문으로 화면 전환(#22)

--- feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/DetailStudyScreen.kt
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/DetailStudyScreen.kt
@@ -1,5 +1,6 @@
 package kr.boostcamp_2024.course.study.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
@@ -9,16 +10,12 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.navigation.NavDestination.Companion.hasRoute
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
 import kr.boostcamp_2024.course.study.R
 import kr.boostcamp_2024.course.study.component.DetailStudyTopBar
 import kr.boostcamp_2024.course.study.navigation.DetailScreenRoute
@@ -30,39 +27,26 @@ fun DetailStudyScreen(
     onCreateCategoryButtonClick: () -> Unit,
     onCategoryClick: () -> Unit
 ) {
-    val bottomNavController = rememberNavController()
+    var selectedScreenIndex by remember { mutableIntStateOf(0) }
+    val screenList = listOf(
+        DetailScreenRoute, GroupScreenRoute
+    )
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         topBar = { DetailStudyTopBar() },
         bottomBar = {
-            val navBackStackEntry by bottomNavController.currentBackStackEntryAsState()
-            val currentDestination = navBackStackEntry?.destination
-            val navList = listOf(
-                DetailScreenRoute, GroupScreenRoute
-            )
             NavigationBar {
-                navList.forEach { nav ->
-                    val selected = currentDestination?.hasRoute(nav::class) ?: false
+                screenList.forEachIndexed { index, screen ->
+                    val selected = selectedScreenIndex == index
                     NavigationBarItem(
                         selected = selected,
                         onClick = {
-                            bottomNavController.navigate(nav) {
-                                // 내비게이션 시작 지점까지 있는 모든 스택을 팝하는 부분
-                                // 탭한 스크린만 남겨놓기 위한 설계
-                                popUpTo(bottomNavController.graph.findStartDestination().id) {
-                                    // 화면 상태 저장하는 부분
-                                    saveState = true
-                                }
-                                // 같은 탭을 또 클릭했을 때 중복으로 스택에 쌓이지 않도록 하는 설정
-                                launchSingleTop = true
-                                // 같은 탭을 또 클릭했을 때 상태 유지를 위한 설정
-                                restoreState = true
-                            }
+                            selectedScreenIndex = index
                         },
-                        label = { Text(text = stringResource(nav.title)) },
+                        label = { Text(text = stringResource(screen.title)) },
                         icon = {
                             Icon(
-                                painter = painterResource(id = nav.iconId),
+                                painter = painterResource(id = screen.iconId),
                                 contentDescription = stringResource(R.string.icon_bottm_nav_detail_study)
                             )
                         }
@@ -71,18 +55,14 @@ fun DetailStudyScreen(
             }
         }
     ) { innerPadding ->
-        NavHost(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            navController = bottomNavController,
-            startDestination = DetailScreenRoute
+                .padding(innerPadding)
         ) {
-            composable<DetailScreenRoute> {
-                CategoryListScreen()
-            }
-            composable<GroupScreenRoute> {
-                GroupListScreen()
+            when (selectedScreenIndex) {
+                0 -> CategoryListScreen()
+                1 -> GroupListScreen()
             }
         }
     }
