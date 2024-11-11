91cee785bf34a39edcd164440e010553ed96df25
천일영 <147039081+Cheonilyeong@users.noreply.github.com>
2024-11-12 03:59:49 +0900

    feat: QuizScreen, QuizRoute 연결(#64)

---
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/viewmodel/QuizViewModel.kt
@@ -0,0 +1,8 @@
+package kr.boostcamp_2024.course.quiz.viewmodel
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class QuizViewModel : ViewModel() {
+}
