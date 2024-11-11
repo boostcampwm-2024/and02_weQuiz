81603efef58ae452fa812b6b2653f0cb762380d5
915dbfl <dbfl00915@naver.com>
2024-11-11 12:44:05 +0900

    refactor: firebaseApp 초기화 코드 application으로 이동(#46)

--- app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/MainActivity.kt
+++ app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.google.firebase.FirebaseApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        FirebaseApp.initializeApp(this)
         enableEdgeToEdge()
         setContent {
             WeQuizApp()
