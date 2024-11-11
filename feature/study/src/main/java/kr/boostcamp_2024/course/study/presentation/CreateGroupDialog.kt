3d971680a9927360faaeb7bc1ccc3d811f0f1116
mk43313@gmail.com <mk43313@gmail.com>
2024-11-12 00:08:24 +0900

    refactor:String 추출, 피드백 반영(#50)

--- feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/CreateGroupDialog.kt
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/CreateGroupDialog.kt
@@ -7,36 +7,40 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizBaseDialog
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizTextField
 import kr.boostcamp_2024.course.study.R
 
 @Composable
-fun CreateGroupScreen(onDismissButtonClick: () -> Unit, onConfirmButtonClick: () -> Unit) {
+fun CreateGroupDialog(onDismissButtonClick: () -> Unit, onConfirmButtonClick: () -> Unit) {
     var email by remember { mutableStateOf("") }
     val isEmailValid = remember(email) { Patterns.EMAIL_ADDRESS.matcher(email).matches() }
     WeQuizBaseDialog(
-        title = "그룹원의 이메일을 입력해주세요!\n초대 메일이 갑니다!",
+        title = stringResource(R.string.dialog_create_group_title),
         dialogImage = painterResource(id = R.drawable.waterfall),
-        confirmTitle = "초대",
-        dismissTitle = "취소",
-        onConfirm = { if (isEmailValid) onConfirmButtonClick() },
+        confirmTitle = stringResource(R.string.btn_dialog_create_group_confirm),
+        dismissTitle = stringResource(R.string.btn_dialog_create_group_dismiss),
+        onConfirm = { onConfirmButtonClick() },
         onDismissRequest = onDismissButtonClick,
         content = {
             WeQuizTextField(
-                label = "그룹원 이메일",
+                label = stringResource(R.string.textfield_label_create_group),
                 text = email,
                 onTextChanged = { email = it },
-                placeholder = "그룹원 이메일을 입력주세요."
+                placeholder = stringResource(R.string.textfield_placeholder_create_group)
             )
         },
         confirmButtonEnabled = isEmailValid
     )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun CreateGroupScreenPreview() {
-    CreateGroupScreen(onConfirmButtonClick = {}, onDismissButtonClick = {})
+    WeQuizTheme {
+        CreateGroupDialog(onConfirmButtonClick = {}, onDismissButtonClick = {})
+    }
 }
