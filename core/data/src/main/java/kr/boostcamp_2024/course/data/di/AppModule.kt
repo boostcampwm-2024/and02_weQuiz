d49033226b1f1654a885abf5f115858952f80fae
915dbfl <dbfl00915@naver.com>
2024-11-12 00:28:50 +0900

    feat: preference dataStore di 로직 작성(#52)

--- core/data/src/main/java/kr/boostcamp_2024/course/data/di/AppModule.kt
+++ core/data/src/main/java/kr/boostcamp_2024/course/data/di/AppModule.kt
@@ -1,15 +1,25 @@
 package kr.boostcamp_2024.course.data.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+
+private val Context.weQuizDataStore: DataStore<Preferences> by preferencesDataStore("wequiz_datastore")
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
     @Provides
     fun provideFirebaseFireStore() = Firebase.firestore
+
+    @Provides
+    fun provideWeQuizDataStore(@ApplicationContext context: Context) = context.weQuizDataStore
 }
