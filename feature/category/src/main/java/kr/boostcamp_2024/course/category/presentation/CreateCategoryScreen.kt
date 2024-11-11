c5aa9aa37313e9aa95261f4cc96d7672861c1dc6
moyeongmin <qa98589858@gmail.com>
2024-11-12 03:22:05 +0900

    feat: 카테고리 생성 UI 작업(#63)

--- feature/category/src/main/java/kr/boostcamp_2024/course/category/presentation/CreateCategoryScreen.kt
+++ feature/category/src/main/java/kr/boostcamp_2024/course/category/presentation/CreateCategoryScreen.kt
@@ -1,37 +1,123 @@
 package kr.boostcamp_2024.course.category.presentation
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.category.R
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizLeftChatBubble
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizLocalRoundedImage
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizTextField
 
+data class CategoryState(
+    val categoryTitle: String = "",
+    val categoryDescription: String = "",
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CreateCategoryScreen(
     onNavigationButtonClick: () -> Unit,
     onCreateCategorySuccess: () -> Unit,
 ) {
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Button(
-            modifier = Modifier.align(Alignment.TopStart),
-            onClick = onNavigationButtonClick
-        ) {
-            Text(text = "뒤로가기 버튼")
-        }
+    var state by remember { mutableStateOf(CategoryState()) }
 
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "카테고리 생성")
+                    //stringResource(R.string.top_app_bar_create_quiz)
+                },
+                navigationIcon = {
+                    IconButton(onClick = onNavigationButtonClick) {
+                        Icon(
+                            imageVector = Icons.AutoMirrored.Default.ArrowBack,
+                            contentDescription = "뒤로 가기"
+                        )
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
         Column(
-            modifier = Modifier.align(Alignment.Center),
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
         ) {
-            Text(text = "카테고리 생성 화면")
-            Button(onClick = onCreateCategorySuccess) {
-                Text(text = "카테고리 생성")
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 20.dp, vertical = 10.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                WeQuizLocalRoundedImage(
+                    modifier = Modifier.size(120.dp),
+                    imagePainter = painterResource(R.drawable.sample_profile),
+                    contentDescription = null
+                )
+                WeQuizLeftChatBubble(text = stringResource(R.string.txt_create_category_guide))
+            }
+
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 20.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ) {
+                WeQuizTextField(
+                    label = stringResource(R.string.txt_create_category_title_label),
+                    text = state.categoryTitle,
+                    maxLines = 1,
+                    minLines = 1,
+                    onTextChanged = { state = state.copy(categoryTitle = it) },
+                    placeholder = stringResource(R.string.txt_create_category_title_placeholder),
+                )
+                WeQuizTextField(
+                    label = stringResource(R.string.txt_create_category_des_label),
+                    text = state.categoryDescription,
+                    maxLines = 99,
+                    minLines = 1,
+                    onTextChanged = { state = state.copy(categoryDescription = it) },
+                    placeholder = stringResource(R.string.txt_create_category_des_placeholder),
+                )
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = onCreateCategorySuccess
+                    /*
+                    TODO 카테고리 생성
+                    state.categoryDescription
+                    state.categoryTitle
+                      */
+                ) {
+                    Text(text = stringResource(R.string.btn_create_category))
+                    //stringResource(R.string.btn_create_quiz)
+                }
             }
         }
     }
