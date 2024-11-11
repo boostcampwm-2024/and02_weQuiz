18739b90ee2a21b1f0ffb1ba21c96663b88a61c0
moyeongmin <qa98589858@gmail.com>
2024-11-11 21:19:19 +0900

    refactor: Question height 설정(#31)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/Question.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/Question.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.material3.MaterialTheme
@@ -13,7 +14,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -22,8 +22,8 @@ fun Question(
     onOptionSelected: (Int) -> Unit
 ) {
     Column(
-        modifier = Modifier.padding(horizontal = 16.dp),
-        verticalArrangement = Arrangement.spacedBy(10.dp)
+        modifier = Modifier.padding(horizontal = 16.dp).height(900.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp),
     ) {
         //TODO(객관식 문항 통신으로 받아오기)
         val options = listOf(
