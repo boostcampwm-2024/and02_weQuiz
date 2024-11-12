6201cbab4efd5733109b973d8035e261db2688b5
915dbfl <dbfl00915@naver.com>
2024-11-12 19:33:30 +0900

    fix: QuizViewModel hilt 적용 에러 해결(#73)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/viewmodel/QuizViewModel.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/viewmodel/QuizViewModel.kt
@@ -2,7 +2,8 @@ package kr.boostcamp_2024.course.quiz.viewmodel
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class QuizViewModel : ViewModel() {
+class QuizViewModel @Inject constructor() : ViewModel() {
 }
