18c4c088cc876b78783ac4fe6cf8d865d69c7649
mk43313@gmail.com <mk43313@gmail.com>
2024-11-13 02:59:08 +0900

    feat: Category Repo Implementation 생성(#81)

--- core/data/src/main/java/kr/boostcamp_2024/course/data/repository/CategoryRepositoryImpl.kt
+++ core/data/src/main/java/kr/boostcamp_2024/course/data/repository/CategoryRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package kr.boostcamp_2024.course.data.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import kr.boostcamp_2024.course.data.model.CategoryDTO
+import kr.boostcamp_2024.course.domain.model.Category
 import kr.boostcamp_2024.course.domain.repository.CategoryRepository
 import javax.inject.Inject
 
@@ -8,4 +11,13 @@ class CategoryRepositoryImpl @Inject constructor(
     firestore: FirebaseFirestore,
 ) : CategoryRepository {
     private val categoryCollectionRef = firestore.collection("Category")
+
+    override suspend fun getCategories(categoryIds: List<String>): Result<List<Category>> =
+        runCatching {
+            categoryIds.map { categoryIds ->
+                val document = categoryCollectionRef.document(categoryIds).get().await()
+                val response = document.toObject(CategoryDTO::class.java)
+                requireNotNull(response).toVO(categoryIds)
+            }
+        }
 }
