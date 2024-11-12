79f1056f872dc4754c5aca8665a266a0598b7ea5
mk43313@gmail.com <mk43313@gmail.com>
2024-11-13 03:05:12 +0900

    refactor: CategoryListScreen에 Categories 정보 적용(#81)

--- feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/CategoryListScreen.kt
+++ feature/study/src/main/java/kr/boostcamp_2024/course/study/presentation/CategoryListScreen.kt
@@ -5,18 +5,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AddCircle
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import kr.boostcamp_2024.course.domain.model.Category
 import kr.boostcamp_2024.course.study.R
 import kr.boostcamp_2024.course.study.component.CategoryItem
 import kr.boostcamp_2024.course.study.component.CustomPropertyTab
 
 @Composable
-fun CategoryListScreen(createCategoryClick: () -> Unit, categoryItemClick: () -> Unit) {
+// TODO : categories 적용하기
+fun CategoryListScreen(categories: List<Category>, createCategoryClick: () -> Unit, categoryItemClick: () -> Unit) {
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -27,25 +30,16 @@ fun CategoryListScreen(createCategoryClick: () -> Unit, categoryItemClick: () ->
             imageVector = Icons.Outlined.AddCircle,
             title = R.string.property_tab_category_text,
         )
-        CategoryLazyColumn(categoryItemClick)
+        CategoryLazyColumn(categories, categoryItemClick)
     }
 }
 
 @Composable
-fun CategoryLazyColumn(categoryItemClick: () -> Unit) {
+fun CategoryLazyColumn(categories: List<Category>, categoryItemClick: () -> Unit) {
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
-        items(10) { index ->
-            CategoryItem(
-                onClicked = categoryItemClick,
-                title = "스레드",
-                content = "카테고리 소개를 적어주세요. 카테고리 소개를 적어주세요. 카테고리 소개를 적어주세요. 카테고리 소개를 적어주세요. 카테고리 소개를 적어주세요. 카테고리 소개를 적어주세요. 카테고리 소개를 적어주세요. 카테고리 소개를 적어주세요.",
-                author = "내가 퀴즈 낸 사람이다.",
-                quizCount = 3,
-                categoryImgUrl = null,
-                profileImgUrl = null,
-            )
-
-            if (index < 9) {
+        itemsIndexed(items = categories, key = { _, category -> category.id }) { index, category ->
+            CategoryItem(categoryItemClick, categories.size, category, "", "")
+            if (index < categories.size) {
                 HorizontalDivider()
             }
         }
