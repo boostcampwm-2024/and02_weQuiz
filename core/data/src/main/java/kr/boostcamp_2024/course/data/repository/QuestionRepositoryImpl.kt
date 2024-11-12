4281c57bd5aa9c54060a072cb3da53447af35d45
915dbfl <dbfl00915@naver.com>
2024-11-12 18:25:39 +0900

    feat: create question repository 로직 작성(#71)

--- core/data/src/main/java/kr/boostcamp_2024/course/data/repository/QuestionRepositoryImpl.kt
+++ core/data/src/main/java/kr/boostcamp_2024/course/data/repository/QuestionRepositoryImpl.kt
@@ -1,6 +1,9 @@
 package kr.boostcamp_2024.course.data.repository
 
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import kr.boostcamp_2024.course.data.model.toDTO
+import kr.boostcamp_2024.course.domain.model.QuestionCreationInfo
 import kr.boostcamp_2024.course.domain.repository.QuestionRepository
 import javax.inject.Inject
 
@@ -8,4 +11,11 @@ class QuestionRepositoryImpl @Inject constructor(
     firestore: FirebaseFirestore,
 ) : QuestionRepository {
     private val questionCollectionRef = firestore.collection("Question")
+
+    override suspend fun createQuestion(questionCreationInfo: QuestionCreationInfo): Result<String> {
+        return runCatching {
+            val document = questionCollectionRef.add(questionCreationInfo.toDTO()).await()
+            document.id
+        }
+    }
 }
