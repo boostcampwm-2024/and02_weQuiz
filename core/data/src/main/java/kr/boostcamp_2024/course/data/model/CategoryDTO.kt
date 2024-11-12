e2ed8c6453835b2bff9187451140e829a79c48e1
mk43313@gmail.com <mk43313@gmail.com>
2024-11-13 02:58:22 +0900

    feat: Category 모델 생성(#81)

---
+++ core/data/src/main/java/kr/boostcamp_2024/course/data/model/CategoryDTO.kt
@@ -0,0 +1,21 @@
+package kr.boostcamp_2024.course.data.model
+
+import com.google.firebase.firestore.PropertyName
+import kr.boostcamp_2024.course.domain.model.Category
+
+data class CategoryDTO(
+    val name: String? = null,
+    val description: String? = null,
+    @get:PropertyName("category_image_url")
+    @set:PropertyName("category_image_url")
+    var categoryImageUrl: String? = null,
+    val quizzes: List<String>? = null,
+) {
+    fun toVO(categoryId: String): Category = Category(
+        id = categoryId,
+        name = requireNotNull(name),
+        description = description,
+        categoryImageUrl = categoryImageUrl,
+        quizzes = emptyList(),
+    )
+}
