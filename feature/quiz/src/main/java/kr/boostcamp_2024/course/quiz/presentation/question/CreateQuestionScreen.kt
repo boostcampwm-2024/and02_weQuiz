de3f8a4a520f016ecd3bc51356d38c55064360c4
915dbfl <dbfl00915@naver.com>
2024-11-12 22:08:25 +0900

    feat: create question screen에 공통 컴포넌트 적용(#71)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/question/CreateQuestionScreen.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/question/CreateQuestionScreen.kt
@@ -1,6 +1,5 @@
 package kr.boostcamp_2024.course.quiz.presentation.question
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,9 +9,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -21,8 +18,6 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
@@ -34,8 +29,6 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,11 +36,13 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizLeftChatBubble
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizLocalRoundedImage
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizOutLinedTextField
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizTextField
 import kr.boostcamp_2024.course.quiz.CreateQuestionUiState
 import kr.boostcamp_2024.course.quiz.CreateQuestionViewModel
 import kr.boostcamp_2024.course.quiz.R
-import kr.boostcamp_2024.course.quiz.presentation.component.ChatBubble
-import kr.boostcamp_2024.course.quiz.presentation.component.WeQuizTextField
 
 @Composable
 fun CreateQuestionScreen(
@@ -116,53 +111,61 @@ fun CreateQuestionScreen(
             )
         },
     ) { innerPadding ->
-        val scrollState = rememberScrollState()
         Box(
             modifier = Modifier
                 .padding(innerPadding)
-                .fillMaxSize()
+                .fillMaxSize(),
         ) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .verticalScroll(scrollState),
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
             ) {
-                CreateQuestionGuideContent(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                )
-                CreateQuestionContent(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    title = uiState.questionCreationInfo.title,
-                    description = uiState.questionCreationInfo.description,
-                    solution = uiState.questionCreationInfo.solution,
-                    onTitleChanged = onTitleChanged,
-                    onDescriptionChanged = onDescriptionChanged,
-                    onSolutionChanged = onSolutionChanged,
-                )
-                CreateChoiceItems(
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    choices = uiState.questionCreationInfo.choices,
-                    selectedChoiceNum = uiState.questionCreationInfo.answer,
-                    updateChoiceText = onChoiceTextChanged,
-                    updateSelectedChoiceNum = onSelectedChoiceNumChanged,
-                )
-                Button(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    enabled = uiState.isCreateQuestionValid,
-                    onClick = onCreateQuestionButtonClick,
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.btn_create_question),
+                item {
+                    CreateQuestionGuideContent(
+                        modifier = Modifier.padding(horizontal = 16.dp),
                     )
+                }
+
+                item {
+                    CreateQuestionContent(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        title = uiState.questionCreationInfo.title,
+                        description = uiState.questionCreationInfo.description,
+                        solution = uiState.questionCreationInfo.solution,
+                        onTitleChanged = onTitleChanged,
+                        onDescriptionChanged = onDescriptionChanged,
+                        onSolutionChanged = onSolutionChanged,
+                    )
+                }
+
+                item {
+                    CreateChoiceItems(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        choices = uiState.questionCreationInfo.choices,
+                        selectedChoiceNum = uiState.questionCreationInfo.answer,
+                        updateChoiceText = onChoiceTextChanged,
+                        updateSelectedChoiceNum = onSelectedChoiceNumChanged,
+                    )
+                }
+
+                item {
+                    Button(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 16.dp),
+                        enabled = uiState.isCreateQuestionValid,
+                        onClick = onCreateQuestionButtonClick,
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.btn_create_question),
+                        )
+                    }
                 }
             }
             if (uiState.isLoading) {
                 CircularProgressIndicator(
                     modifier = Modifier
                         .size(64.dp)
-                        .align(Alignment.Center)
+                        .align(Alignment.Center),
                 )
             }
         }
@@ -179,22 +182,19 @@ fun CreateQuestionGuideContent(
             .padding(vertical = 10.dp),
         horizontalArrangement = Arrangement.spacedBy(10.dp),
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.img_clock_character),
+        WeQuizLocalRoundedImage(
+            modifier = Modifier.size(120.dp),
+            imagePainter = painterResource(id = R.drawable.img_clock_character),
             contentDescription = null,
-            modifier = Modifier
-                .size(120.dp)
-                .clip(CircleShape),
-            contentScale = ContentScale.Crop,
         )
         Column(
             modifier = Modifier.weight(1f),
             verticalArrangement = Arrangement.spacedBy(6.dp),
         ) {
-            ChatBubble(
+            WeQuizLeftChatBubble(
                 text = stringResource(id = R.string.txt_create_question_guide1),
             )
-            ChatBubble(
+            WeQuizLeftChatBubble(
                 text = stringResource(id = R.string.txt_create_question_guide2),
             )
         }
@@ -226,14 +226,16 @@ fun CreateQuestionContent(
             text = description ?: "",
             onTextChanged = onDescriptionChanged,
             placeholder = stringResource(id = R.string.txt_question_content_placeholder),
-            minLine = 6,
+            minLines = 6,
+            maxLines = 6,
         )
         WeQuizTextField(
             label = stringResource(id = R.string.txt_question_description_label),
             text = solution ?: "",
             onTextChanged = onSolutionChanged,
             placeholder = stringResource(id = R.string.txt_question_description_placeholder),
-            minLine = 6,
+            minLines = 6,
+            maxLines = 6,
         )
     }
 }
@@ -281,23 +283,13 @@ fun ChoiceItem(
                 onSelected(!isSelected)
             },
         )
-        OutlinedTextField(
-            value = text,
-            onValueChange = { onTextChanged(it) },
+        WeQuizOutLinedTextField(
             modifier = Modifier.weight(1f),
-            textStyle = MaterialTheme.typography.bodyLarge,
-            trailingIcon = {
-                IconButton(
-                    onClick = { onTextChanged("") },
-                ) {
-                    Icon(
-                        painter = painterResource(R.drawable.outline_cancel_on_surface_variant),
-                        tint = MaterialTheme.colorScheme.onSurfaceVariant,
-                        contentDescription = stringResource(id = R.string.des_clear_text),
-                    )
-                }
-            },
-            placeholder = { Text(stringResource(id = R.string.txt_question_choice_placeholder)) },
+            text = text,
+            onTextChanged = onTextChanged,
+            placeholder = stringResource(id = R.string.txt_question_choice_placeholder),
+            minLines = 1,
+            maxLines = 1,
         )
     }
 }
