9301c446d11d4f9cc9bb548af128193d6da97af6
915dbfl <dbfl00915@naver.com>
2024-11-13 11:31:06 +0900

    refactor: WeQuizChatBubble 정렬 수정(#52)

--- feature/login/src/main/java/kr/boostcamp_2024/course/login/presentation/LoginScreen.kt
+++ feature/login/src/main/java/kr/boostcamp_2024/course/login/presentation/LoginScreen.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -106,12 +108,24 @@ fun LoginGuideImageAndText() {
             contentDescription = null,
             contentScale = ContentScale.FillWidth
         )
-        WeQuizLeftChatBubble(
-            text = stringResource(R.string.txt_introduce_app1),
-        )
-        WeQuizRightChatBubble(
-            text = stringResource(R.string.txt_introduce_app2),
-        )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Start,
+        ) {
+            WeQuizLeftChatBubble(
+                modifier = Modifier.wrapContentSize(),
+                text = stringResource(R.string.txt_introduce_app1),
+            )
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.End,
+        ) {
+            WeQuizRightChatBubble(
+                modifier = Modifier.wrapContentSize(),
+                text = stringResource(R.string.txt_introduce_app2),
+            )
+        }
     }
 }
 
