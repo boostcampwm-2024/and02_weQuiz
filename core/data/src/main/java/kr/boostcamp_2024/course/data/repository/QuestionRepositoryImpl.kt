27d45ae7ba4e43a4228a9ba6ba26a9acf5c3d812
moyeongmin <qa98589858@gmail.com>
2024-11-13 10:51:59 +0900

    feat: QuestionRepositoryImpl 작성(#77)

--- core/data/src/main/java/kr/boostcamp_2024/course/data/repository/QuestionRepositoryImpl.kt
+++ core/data/src/main/java/kr/boostcamp_2024/course/data/repository/QuestionRepositoryImpl.kt
@@ -1,6 +1,11 @@
 package kr.boostcamp_2024.course.data.repository
 
+import android.util.Log
+import com.google.firebase.firestore.FieldPath
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.coroutines.tasks.await
+import kr.boostcamp_2024.course.domain.model.Question
+import kr.boostcamp_2024.course.data.model.QuestionDTO
 import kr.boostcamp_2024.course.domain.repository.QuestionRepository
 import javax.inject.Inject
 
@@ -8,4 +13,26 @@ class QuestionRepositoryImpl @Inject constructor(
     firestore: FirebaseFirestore,
 ) : QuestionRepository {
     private val questionCollectionRef = firestore.collection("Question")
+
+    override suspend fun getQuestions(questionIds: List<String>): Result<List<Question>> {
+        return runCatching {
+            Log.d("QuestionRepositoryImpl", "getQuestions: $questionIds")
+            val snapshot = questionCollectionRef.whereIn(FieldPath.documentId(), questionIds).get().await()
+            snapshot.documents.mapNotNull { document ->
+                document.toObject(QuestionDTO::class.java)?.toVO()
+            }
+        }
+    }
+
+//    override suspend fun submitQuiz(quizSubmission: QuizSubmission): Result<Boolean> {
+//        return runCatching {
+//            val quizDocument = quizCollectionRef.document(quizSubmission.quizId)
+//            val userAnswers = mapOf(
+//                "userId" to quizSubmission.userId,
+//                "answers" to quizSubmission.answers
+//            )
+//            quizDocument.collection("user_omr").add(userAnswers).await()
+//            true
+//        }
+//    }
 }
