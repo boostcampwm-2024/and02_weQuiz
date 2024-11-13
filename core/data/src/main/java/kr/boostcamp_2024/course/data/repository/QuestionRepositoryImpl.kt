96f6d02b1f340ef8d949906cc79157a73a553fe1
moyeongmin <qa98589858@gmail.com>
2024-11-13 18:03:24 +0900

    feat: QuestionRepositoyImpl 작성(#84)

--- core/data/src/main/java/kr/boostcamp_2024/course/data/repository/QuestionRepositoryImpl.kt
+++ core/data/src/main/java/kr/boostcamp_2024/course/data/repository/QuestionRepositoryImpl.kt
@@ -1,7 +1,10 @@
 package kr.boostcamp_2024.course.data.repository
 
+import android.provider.SyncStateContract.Helpers.update
 import android.util.Log
+import com.google.android.gms.tasks.Tasks.await
 import com.google.firebase.firestore.FieldPath
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.coroutines.tasks.await
 import kr.boostcamp_2024.course.data.model.QuestionDTO
@@ -16,6 +19,7 @@ class QuestionRepositoryImpl @Inject constructor(
 ) : QuestionRepository {
     private val questionCollectionRef = firestore.collection("Question")
     private val userOmrCollectionRef = firestore.collection("UserOmr")
+    private val quizCollectionRef = firestore.collection("Quiz")
 
     override suspend fun getQuestions(questionIds: List<String>): Result<List<Question>> {
         return runCatching {
@@ -35,5 +39,11 @@ class QuestionRepositoryImpl @Inject constructor(
                 answers = userOmr.answers,
             )
             userOmrCollectionRef.add(userOmrDTO).await().id
+        }
+    override suspend fun addUserOmrToQuiz(quizId: String, userOmrId: String): Result<Unit> =
+        runCatching {
+           quizCollectionRef.document(quizId)
+                .update("user_omrs", FieldValue.arrayUnion(userOmrId))
+                .await()
         }
 }
