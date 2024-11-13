fba1b7792791129a09f6018f2303b49fc3389b42
moyeongmin <qa98589858@gmail.com>
2024-11-13 18:03:05 +0900

    feat: QuestionRepository 작성(#84)

--- core/domain/src/main/java/kr/boostcamp_2024/course/domain/repository/QuestionRepository.kt
+++ core/domain/src/main/java/kr/boostcamp_2024/course/domain/repository/QuestionRepository.kt
@@ -6,4 +6,5 @@ import kr.boostcamp_2024.course.domain.model.UserOmr
 interface QuestionRepository {
     suspend fun getQuestions(questionIds: List<String>): Result<List<Question>>
     suspend fun submitQuiz(userOmr: UserOmr): Result<String>
+    suspend fun addUserOmrToQuiz(quizId: String, userOmrId: String): Result<Unit>
 }
