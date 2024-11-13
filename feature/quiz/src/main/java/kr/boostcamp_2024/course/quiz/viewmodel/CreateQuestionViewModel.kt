399b872d31b70cb2314133a91a95871e124177a0
915dbfl <dbfl00915@naver.com>
2024-11-13 17:39:04 +0900

    refactor: CreateQuestionViewModel ktlint 오류 해결

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/viewmodel/CreateQuestionViewModel.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/viewmodel/CreateQuestionViewModel.kt
@@ -98,7 +98,7 @@ class CreateQuestionViewModel @Inject constructor(
         _createQuestionUiState.update { currentState ->
             currentState.copy(
                 isCreateQuestionValid = currentState.questionCreationInfo.title.isNotBlank() &&
-                        currentState.questionCreationInfo.choices.all { it.isNotBlank() },
+                    currentState.questionCreationInfo.choices.all { it.isNotBlank() },
             )
         }
     }
