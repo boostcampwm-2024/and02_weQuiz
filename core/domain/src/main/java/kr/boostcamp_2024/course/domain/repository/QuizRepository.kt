5e100c79576edab4af7292ff721f041cf0a09ed7
915dbfl <dbfl00915@naver.com>
2024-11-13 16:58:09 +0900

    refactor: ktlint 적용(#71)

--- core/domain/src/main/java/kr/boostcamp_2024/course/domain/repository/QuizRepository.kt
+++ core/domain/src/main/java/kr/boostcamp_2024/course/domain/repository/QuizRepository.kt
@@ -4,5 +4,6 @@ import kr.boostcamp_2024.course.domain.model.Quiz
 
 interface QuizRepository {
     suspend fun getQuiz(quizId: String): Result<Quiz>
+
     suspend fun updateQuizQuestionList(quizId: String, questionList: List<String>): Result<Unit>
 }
