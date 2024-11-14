f04ab07b8ecf3bf1158c1d5abcf9c4aca7ec5100
915dbfl <dbfl00915@naver.com>
2024-11-15 04:29:51 +0900

    feat: 퀴즈 카테고리 추가 로직 적용(#113)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/viewmodel/CreateQuizViewModel.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/viewmodel/CreateQuizViewModel.kt
@@ -1,15 +1,19 @@
 package kr.boostcamp_2024.course.quiz.viewmodel
 
 import android.util.Log
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.toRoute
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kr.boostcamp_2024.course.domain.model.QuizCreationInfo
+import kr.boostcamp_2024.course.domain.repository.CategoryRepository
 import kr.boostcamp_2024.course.domain.repository.QuizRepository
+import kr.boostcamp_2024.course.quiz.navigation.CreateQuizRoute
 import javax.inject.Inject
 
 data class CreateQuizUiState(
@@ -18,6 +22,8 @@ data class CreateQuizUiState(
     val quizDate: String = "",
     val quizSolveTime: Float = 10f,
     val isCreateQuizSuccess: Boolean = false,
+    val isLoading: Boolean = false,
+    val snackBarMessage: String? = null,
 ) {
     val isCreateQuizButtonEnabled: Boolean
         get() = quizTitle.isNotBlank() && quizDate.isNotBlank() && quizSolveTime > 0
@@ -25,10 +31,13 @@ data class CreateQuizUiState(
 
 @HiltViewModel
 class CreateQuizViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
     private val quizRepository: QuizRepository,
+    private val categoryRepository: CategoryRepository,
 ) : ViewModel() {
     private val _uiState = MutableStateFlow(CreateQuizUiState())
     val uiState = _uiState.asStateFlow()
+    private val categoryId = savedStateHandle.toRoute<CreateQuizRoute>().categoryId
 
     fun setQuizTitle(quizTitle: String) {
         _uiState.update { it.copy(quizTitle = quizTitle) }
@@ -47,6 +56,7 @@ class CreateQuizViewModel @Inject constructor(
     }
 
     fun createQuiz() {
+        setLoadingState()
         viewModelScope.launch {
             quizRepository.createQuiz(
                 QuizCreationInfo(
@@ -55,12 +65,44 @@ class CreateQuizViewModel @Inject constructor(
                     quizDate = uiState.value.quizDate,
                     quizSolveTime = uiState.value.quizSolveTime.toInt(),
                 ),
+            ).onSuccess { quizId ->
+                saveQuizToCategory(quizId)
+            }.onFailure {
+                Log.e("CreateQuizViewModel", it.message, it)
+                setNewSnackBarMessage("퀴즈 생성에 실패했습니다.")
+            }
+        }
+    }
+
+    private suspend fun saveQuizToCategory(quizId: String) {
+        try {
+            categoryRepository.addQuizToCategory(categoryId, quizId).getOrThrow()
+
+            _uiState.update {
+                it.copy(
+                    isLoading = false,
+                    isCreateQuizSuccess = true,
+                )
+            }
+        } catch (exception: Exception) {
+            Log.e("CreateQuizViewModel", exception.message, exception)
+            setNewSnackBarMessage("퀴즈 저장에 실패했습니다.")
+        }
+    }
+
+    private fun setLoadingState() {
+        _uiState.update { currentState ->
+            currentState.copy(
+                isLoading = true,
             )
-                .onSuccess { quizId ->
-                    Log.d("CreateQuizViewModel", quizId)
-                    _uiState.update { it.copy(isCreateQuizSuccess = true) }
-                }
-                .onFailure { /* error */ }
+        }
+    }
+
+    fun setNewSnackBarMessage(message: String?) {
+        _uiState.update { currentState ->
+            currentState.copy(
+                snackBarMessage = message,
+            )
         }
     }
 }
