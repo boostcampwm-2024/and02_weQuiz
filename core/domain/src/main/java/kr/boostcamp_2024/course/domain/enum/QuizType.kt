d393f934b3eb285db746b3d13eea529a18e5d547
915dbfl <dbfl00915@naver.com>
2024-11-20 18:51:53 +0900

    feat: quizType 정의(#133)
    Co-authored-by: pengcon <vpdzjs@gmail.com>

---
+++ core/domain/src/main/java/kr/boostcamp_2024/course/domain/enum/QuizType.kt
@@ -0,0 +1,18 @@
+package kr.boostcamp_2024.course.domain.enum
+
+sealed class QuizType(val value: String) {
+    data object RealTime : QuizType("realTime")
+
+    data object General : QuizType("general")
+
+    companion object {
+        fun getQuizTypeFromValue(value: String): QuizType {
+            return when (value) {
+                "realTime" -> RealTime
+                "general" -> General
+                else -> throw IllegalArgumentException("Invalid QuizType value: $value")
+            }
+        }
+
+    }
+}
