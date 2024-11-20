f6b1b8f5447c9fd151e1ac6817ef66ada637971f
915dbfl <dbfl00915@naver.com>
2024-11-20 19:09:52 +0900

    feat: ktlint 오류 해결(#133)
    Co-authored-by: pengcon <vpdzjs@gmail.com>

--- core/domain/src/main/java/kr/boostcamp_2024/course/domain/enum/QuizType.kt
+++ core/domain/src/main/java/kr/boostcamp_2024/course/domain/enum/QuizType.kt
@@ -1,18 +1,17 @@
 package kr.boostcamp_2024.course.domain.enum
 
-sealed class QuizType(val value: String) {
+sealed class QuizType(
+    val value: String,
+) {
     data object RealTime : QuizType("realTime")
 
     data object General : QuizType("general")
 
     companion object {
-        fun getQuizTypeFromValue(value: String): QuizType {
-            return when (value) {
-                "realTime" -> RealTime
-                "general" -> General
-                else -> throw IllegalArgumentException("Invalid QuizType value: $value")
-            }
+        fun getQuizTypeFromValue(value: String): QuizType = when (value) {
+            "realTime" -> RealTime
+            "general" -> General
+            else -> throw IllegalArgumentException("Invalid QuizType value: $value")
         }
-
     }
 }
