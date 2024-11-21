79b469a5c28ae433da3d2b0be473f0b8901ff194
moyeongmin <qa98589858@gmail.com>
2024-11-21 18:32:38 +0900

    feat: QuizRepository 작성(#135)

--- core/domain/src/main/java/kr/boostcamp_2024/course/domain/repository/QuizRepository.kt
+++ core/domain/src/main/java/kr/boostcamp_2024/course/domain/repository/QuizRepository.kt
@@ -10,7 +10,7 @@ interface QuizRepository {
 
     suspend fun addQuestionToQuiz(quizId: String, questionId: String): Result<Unit>
 
-    suspend fun createQuiz(quizCreateInfo: QuizCreationInfo): Result<String>
+    suspend fun createQuiz(quizCreateInfo: QuizCreationInfo, ownerId: String?): Result<String>
 
     suspend fun addUserOmrToQuiz(quizId: String, userOmrId: String): Result<Unit>
 
