abe877ee4eaba39661e6a260101467fbc950be6e
915dbfl <dbfl00915@naver.com>
2024-11-25 17:52:52 +0900

    feat: choiceQuestion / blankQuestion 퀴즈 채점 로직 분리 및 추가 구현(#142)
    Co-authored-by: pengcon <vpdzjs@gmail.com>

--- core/domain/src/main/java/kr/boostcamp_2024/course/domain/model/QuizResult.kt
+++ core/domain/src/main/java/kr/boostcamp_2024/course/domain/model/QuizResult.kt
@@ -1,29 +1,47 @@
 package kr.boostcamp_2024.course.domain.model
 
 data class QuizResult(
-    val choiceQuestions: List<ChoiceQuestion>,
-    val userOmrAnswers: List<Int>,
+    val choiceQuestions: List<Question>,
+    val userOmrAnswers: List<Any>,
 ) {
     val totalQuestions: Int
         get() = userOmrAnswers.size
-
-    val correctQuestions: Int
-        get() = userOmrAnswers.withIndex().count { (index, userAnswer) ->
-            userAnswer == choiceQuestions[index].answer
-        }
 
     val questionResults: List<QuestionResult>
         get() = userOmrAnswers.mapIndexed { index, userOmrAnswers ->
             QuestionResult(
                 choiceQuestion = choiceQuestions[index],
                 userAnswer = userOmrAnswers,
-                isCorrect = choiceQuestions[index].answer == userOmrAnswers,
+                isCorrect = when (userOmrAnswers) {
+                    is Int -> evaluateChoiceQuestion(index, userOmrAnswers)
+                    is Map<*, *> -> evaluateBlankQuestion(index, userOmrAnswers)
+                    else -> throw Exception("Invalid user answer type")
+                },
             )
         }
+
+    val correctQuestions: Int
+        get() = questionResults.count { it.isCorrect }
+
+    private fun evaluateChoiceQuestion(
+        index: Int,
+        userAnswer: Int,
+    ): Boolean = userAnswer == (choiceQuestions[index] as ChoiceQuestion).answer
+
+    private fun evaluateBlankQuestion(
+        index: Int,
+        userAnswer: Map<*, *>,
+    ): Boolean {
+        val blankQuestion = choiceQuestions[index] as BlankQuestion
+        val blankQuestionContent = blankQuestion.questionContent.filterIsInstance<BlankContent>()
+        return blankQuestionContent.withIndex().all { (index, content) ->
+            content.text == userAnswer[index]
+        }
+    }
 }
 
 data class QuestionResult(
-    val choiceQuestion: ChoiceQuestion,
-    val userAnswer: Int,
+    val choiceQuestion: Question,
+    val userAnswer: Any,
     val isCorrect: Boolean,
 )
