666764b55ff98002a4d4b77bdd1d221e13d1756f
pengcon <vpdzjs@gmail.com>
2024-11-26 16:24:34 +0900

    feat: CreateQuestionScreen에 낱말 맞추기 UI 추가 (#144)
    Co-authored-by: 915dbfl <dbfl00915@naver.com>

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/question/CreateQuestionScreen.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/presentation/question/CreateQuestionScreen.kt
@@ -1,8 +1,11 @@
 package kr.boostcamp_2024.course.quiz.presentation.question
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -17,9 +20,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
@@ -31,6 +38,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -43,10 +51,13 @@ import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizLeftChatBubble
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizLocalRoundedImage
 import kr.boostcamp_2024.course.quiz.R
+import kr.boostcamp_2024.course.quiz.component.ConsumeBlankContentUi
+import kr.boostcamp_2024.course.quiz.component.ConsumeTextContentUi
 import kr.boostcamp_2024.course.quiz.component.CreateChoiceItems
 import kr.boostcamp_2024.course.quiz.component.CreateQuestionContent
 import kr.boostcamp_2024.course.quiz.viewmodel.CreateQuestionUiState
 import kr.boostcamp_2024.course.quiz.viewmodel.CreateQuestionViewModel
+import kr.boostcamp_2024.course.quiz.viewmodel.BlankQuestionItem
 
 @Composable
 fun CreateQuestionScreen(
@@ -57,7 +68,10 @@ fun CreateQuestionScreen(
     val uiState by viewModel.createQuestionUiState.collectAsStateWithLifecycle()
     val snackBarHostState = remember { SnackbarHostState() }
     val focusRequester = remember { FocusRequester() }
-
+    val options = listOf(
+        stringResource(R.string.txt_create_general_question),
+        stringResource(R.string.txt_create_blank_question),
+    )
     LaunchedEffect(uiState) {
         if (uiState.creationSuccess) {
             onCreateQuestionSuccess()
@@ -79,10 +93,22 @@ fun CreateQuestionScreen(
         onChoiceTextChanged = viewModel::onChoiceTextChanged,
         onSelectedChoiceNumChanged = viewModel::onSelectedChoiceNumChanged,
         onCreateQuestionButtonClick = viewModel::createQuestion,
+        options = options,
+        onQuestionTypeIndexChange = viewModel::onQuestionTypeIndexChange,
+        selectedQuestionTypeIndex = uiState.selectedQuestionTypeIndex,
+        isBlankQuestion = uiState.isBlankQuestion,
+        blankQuestionItems = uiState.items,
+        onAddBlankItemButtonClick = viewModel::addBlankItem,
+        onAddTextItemButtonClick = viewModel::addTextItem,
+        onBlankQuestionItemValueChanged = viewModel::onBlankQuestionItemValueChanged,
+        onContentRemove = viewModel::onContentRemove,
+        onCreateBlankQuestionButtonClick = viewModel::onCreateBlankQuestion,
+        isCreateBlankButtonValid = uiState.isCreateBlankButtonValid,
+        isCreateTextButtonValid = uiState.isCreateTextButtonValid,
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun CreateQuestionScreen(
     uiState: CreateQuestionUiState,
@@ -95,6 +121,18 @@ fun CreateQuestionScreen(
     onChoiceTextChanged: (Int, String) -> Unit,
     onSelectedChoiceNumChanged: (Int) -> Unit,
     onCreateQuestionButtonClick: () -> Unit,
+    onQuestionTypeIndexChange: (Int) -> Unit,
+    selectedQuestionTypeIndex: Int,
+    options: List<String>,
+    isBlankQuestion: Boolean,
+    blankQuestionItems: List<BlankQuestionItem>,
+    onAddBlankItemButtonClick: () -> Unit,
+    onAddTextItemButtonClick: () -> Unit,
+    onBlankQuestionItemValueChanged: (String, Int) -> Unit,
+    onContentRemove: (Int) -> Unit,
+    onCreateBlankQuestionButtonClick: () -> Unit,
+    isCreateBlankButtonValid: Boolean,
+    isCreateTextButtonValid: Boolean,
 ) {
     val focusManager = LocalFocusManager.current
 
@@ -120,6 +158,23 @@ fun CreateQuestionScreen(
             )
         },
     ) { innerPadding ->
+        SingleChoiceSegmentedButtonRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 10.dp),
+        ) {
+            options.forEachIndexed { index, label ->
+                SegmentedButton(
+                    shape = SegmentedButtonDefaults.itemShape(index = index, count = options.size),
+                    onClick = { },
+                    selected = index == 1,
+                ) {
+                    Text(
+                        text = label,
+                    )
+                }
+            }
+        }
         Box(
             modifier = Modifier
                 .padding(innerPadding)
@@ -129,9 +184,25 @@ fun CreateQuestionScreen(
                 modifier = Modifier.fillMaxSize(),
             ) {
                 item {
-                    CreateQuestionGuideContent(
-                        modifier = Modifier.padding(horizontal = 16.dp),
-                    )
+                    SingleChoiceSegmentedButtonRow(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 65.dp, vertical = 10.dp),
+                    ) {
+                        options.forEachIndexed { index, label ->
+                            SegmentedButton(
+                                shape = SegmentedButtonDefaults.itemShape(index = index, count = options.size),
+                                onClick = {
+                                    onQuestionTypeIndexChange(index)
+                                },
+                                selected = index == selectedQuestionTypeIndex,
+                            ) {
+                                Text(
+                                    text = label,
+                                )
+                            }
+                        }
+                    }
                 }
 
                 item {
@@ -146,31 +217,122 @@ fun CreateQuestionScreen(
                         onTitleChanged = onTitleChanged,
                         onDescriptionChanged = onDescriptionChanged,
                         onSolutionChanged = onSolutionChanged,
-                    )
-                }
+                        isBlankQuestion = isBlankQuestion,
 
-                item {
-                    CreateChoiceItems(
-                        modifier = Modifier.padding(horizontal = 16.dp),
-                        focusManager = focusManager,
-                        choices = uiState.choiceQuestionCreationInfo.choices,
-                        selectedChoiceNum = uiState.choiceQuestionCreationInfo.answer,
-                        updateChoiceText = onChoiceTextChanged,
-                        updateSelectedChoiceNum = onSelectedChoiceNumChanged,
-                    )
-                }
-
-                item {
-                    Button(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(horizontal = 16.dp),
-                        enabled = uiState.isCreateQuestionValid,
-                        onClick = onCreateQuestionButtonClick,
-                    ) {
-                        Text(
-                            text = stringResource(id = R.string.btn_create_question),
                         )
+                }
+
+                if (!isBlankQuestion) {
+                    item {
+                        CreateChoiceItems(
+                            modifier = Modifier.padding(horizontal = 16.dp),
+                            focusManager = focusManager,
+                            choices = uiState.choiceQuestionCreationInfo.choices,
+                            selectedChoiceNum = uiState.choiceQuestionCreationInfo.answer,
+                            updateChoiceText = onChoiceTextChanged,
+                            updateSelectedChoiceNum = onSelectedChoiceNumChanged,
+                        )
+                    }
+                }
+                if (isBlankQuestion) {
+                    item {
+                        Text(
+                            modifier = Modifier.padding(
+                                horizontal = 16.dp,
+                                vertical = 10.dp,
+                            ),
+                            text = "낱말 맞추기 문제 만들기",
+                        )
+                        FlowRow(
+                            modifier = Modifier
+                                .fillMaxSize(1f)
+                                .padding(16.dp)
+                                .background(Color.Gray)
+                                .padding(10.dp),
+                            horizontalArrangement = Arrangement.spacedBy(10.dp),
+                            verticalArrangement = Arrangement.spacedBy(8.dp),
+                        ) {
+                            blankQuestionItems.forEachIndexed { index, it ->
+                                when (it) {
+                                    is BlankQuestionItem.Blank -> {
+                                        ConsumeBlankContentUi(
+                                            word = it.text,
+                                            index = index,
+                                            onContentRemove = onContentRemove,
+                                            onValueChanged = onBlankQuestionItemValueChanged,
+                                        )
+                                    }
+
+                                    is BlankQuestionItem.Text -> {
+                                        ConsumeTextContentUi(
+                                            word = it.text,
+                                            index = index,
+                                            onContentRemove = onContentRemove,
+                                            onValueChanged = onBlankQuestionItemValueChanged,
+
+                                            )
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+
+                    item {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp, Alignment.CenterHorizontally),
+                        ) {
+                            Button(
+                                onClick = onAddTextItemButtonClick,
+                                enabled = isCreateTextButtonValid,
+                            ) {
+                                Text(stringResource(R.string.btn_create_text))
+                            }
+
+                            Button(
+                                onClick = onAddBlankItemButtonClick,
+                                enabled = isCreateBlankButtonValid,
+                            ) {
+                                Text(stringResource(R.string.btn_create_blank))
+                            }
+                        }
+                    }
+                }
+                if (!isBlankQuestion) {
+                    item {
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp),
+                            enabled = uiState.isCreateQuestionValid,
+                            onClick = onCreateQuestionButtonClick,
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.btn_create_question),
+                            )
+                        }
+                    }
+                } else {
+                    item {
+                        HorizontalDivider(
+                            modifier = Modifier.padding(
+                                horizontal = 16.dp,
+                                vertical = 10.dp,
+                            ),
+                        )
+
+                        Button(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp),
+                            enabled = uiState.isCreateBlankQuestionValid,
+                            onClick = onCreateBlankQuestionButtonClick,
+                        ) {
+                            Text(
+                                text = stringResource(id = R.string.btn_create_question),
+                            )
+                        }
                     }
                 }
             }
@@ -184,6 +346,7 @@ fun CreateQuestionScreen(
         }
     }
 }
+
 
 @Composable
 fun CreateQuestionGuideContent(
@@ -213,6 +376,7 @@ fun CreateQuestionGuideContent(
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
