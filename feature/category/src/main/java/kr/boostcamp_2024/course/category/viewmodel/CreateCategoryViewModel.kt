90f5ccc19e2fc95fd3229812f480d26a474c6d7e
915dbfl <dbfl00915@naver.com>
2024-11-27 17:08:28 +0900

    feat: 카테고리 수정 시, 기존 정보 업로드 처리 로직 추가 구현(#159)

--- feature/category/src/main/java/kr/boostcamp_2024/course/category/viewmodel/CreateCategoryViewModel.kt
+++ feature/category/src/main/java/kr/boostcamp_2024/course/category/viewmodel/CreateCategoryViewModel.kt
@@ -21,13 +21,14 @@ data class CreateCategoryUiState(
     val isLoading: Boolean = false,
     val categoryName: String = "",
     val categoryDescription: String = "",
-    val isCategoryCreationValid: Boolean = false,
     val creationSuccess: Boolean = false,
     val errorMessage: String? = null,
-    val categoryImageUrl: String? = null,
     val currentImage: ByteArray? = null,
     val defaultImageUri: String? = null,
-)
+) {
+    val isCategoryCreationValid: Boolean
+        get() = categoryName.isNotBlank()
+}
 
 @HiltViewModel
 class CreateCategoryViewModel @Inject constructor(
@@ -49,6 +50,30 @@ class CreateCategoryViewModel @Inject constructor(
                 else -> updateCategory(categoryId)
             }
 
+        }
+    }
+
+    fun fetchCategoryInfo() {
+        setLoading()
+        viewModelScope.launch {
+            try {
+                categoryRepository.getCategory(requireNotNull(categoryId))
+                    .onSuccess { categoryInfo ->
+                        _createCategoryUiState.update {
+                            it.copy(
+                                isLoading = false,
+                                categoryName = categoryInfo.name,
+                                categoryDescription = categoryInfo.description ?: "",
+                                defaultImageUri = categoryInfo.categoryImageUrl,
+                            )
+                        }
+                    }.onFailure { exception ->
+                        throw exception
+                    }
+            } catch (exception: Exception) {
+                Log.e("CreateCategoryViewModel", "Failed to fetch category info", exception)
+                setErrorMessage("카테고리 정보를 불러오는데 실패했습니다. 다시 시도해주세요!")
+            }
         }
     }
 
@@ -122,21 +147,11 @@ class CreateCategoryViewModel @Inject constructor(
         _createCategoryUiState.update { currentState ->
             currentState.copy(categoryName = name)
         }
-        checkCreateCategoryValid()
     }
 
     fun onDescriptionChanged(description: String) {
         _createCategoryUiState.update { currentState ->
             currentState.copy(categoryDescription = description)
-        }
-        checkCreateCategoryValid()
-    }
-
-    private fun checkCreateCategoryValid() {
-        _createCategoryUiState.update { currentState ->
-            currentState.copy(
-                isCategoryCreationValid = currentState.categoryName.isNotBlank(),
-            )
         }
     }
 
