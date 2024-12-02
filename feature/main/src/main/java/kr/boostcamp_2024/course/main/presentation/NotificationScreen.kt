60c05980c09f6d1d3e740db2ee3d0e652e516313
pengcon <vpdzjs@gmail.com>
2024-12-02 17:53:48 +0900

    fix: 알림이 없을 때 UI 텍스트 그려지지 않던 상황 수정

--- feature/main/src/main/java/kr/boostcamp_2024/course/main/presentation/NotificationScreen.kt
+++ feature/main/src/main/java/kr/boostcamp_2024/course/main/presentation/NotificationScreen.kt
@@ -1,5 +1,6 @@
 package kr.boostcamp_2024.course.main.presentation
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -8,15 +9,19 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizAsyncImage
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizLocalRoundedImage
 import kr.boostcamp_2024.course.domain.model.Notification
 import kr.boostcamp_2024.course.domain.model.NotificationWithGroupInfo
 import kr.boostcamp_2024.course.main.component.NotificationItem
@@ -62,21 +67,32 @@ fun NotificationScreen(
             NotificationTopAppBar(onNavigationButtonClick = onNavigationButtonClick)
         },
     ) { paddingValues ->
-        LazyColumn(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues),
-        ) {
-            items(
-                items = notificationInfos,
-                key = { it.notification.id },
-            ) { notificationInfo ->
-                notificationInfo.studyGroupName ?: return@items
-                NotificationItem(
-                    notificationInfo = notificationInfo,
-                    onRejectClick = { onRejectClick(notificationInfo.notification.id) },
-                    onAcceptClick = { onAcceptClick(notificationInfo.notification) },
-                )
+        if (notificationInfos.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = "알림함이 텅 비었어요!")
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues),
+            ) {
+                items(
+                    items = notificationInfos,
+                    key = { it.notification.id },
+                ) { notificationInfo ->
+                    notificationInfo.studyGroupName ?: return@items
+                    NotificationItem(
+                        notificationInfo = notificationInfo,
+                        onRejectClick = { onRejectClick(notificationInfo.notification.id) },
+                        onAcceptClick = { onAcceptClick(notificationInfo.notification) },
+                    )
+                }
             }
         }
     }
