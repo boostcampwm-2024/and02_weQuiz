2904e288362b3a35adffb71105588168f83cf527
pengcon <vpdzjs@gmail.com>
2025-01-14 19:02:52 +0900

    feat: RealTimeQuizGuideContentPreview 추가 (#196)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/RealTimeQuizGuideContent.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/RealTimeQuizGuideContent.kt
@@ -11,6 +11,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizLocalRoundedImage
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizRightChatBubble
@@ -51,4 +53,15 @@ fun RealTimeQuizGuideContent(
             contentDescription = stringResource(R.string.des_image_question),
         )
     }
+}
+
+@Preview(showBackground = true, locale = "ko")
+@PreviewLightDark
+@Composable
+private fun RealTimeQuizGuideContentPreview() {
+    RealTimeQuizGuideContent(
+        ownerName = "이훈",
+        totalParticipants = 10,
+        submittedParticipants = 5,
+    )
 }
