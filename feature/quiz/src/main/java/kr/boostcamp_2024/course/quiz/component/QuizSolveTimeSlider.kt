062d8821a11b681ba9484f8e29ccf2a87789f866
pengcon <vpdzjs@gmail.com>
2025-01-14 19:00:17 +0900

    refactor: QuizSolveTimeSliderPreview dark / light prievew 추가, 언어 ko로 설정 (#196)

--- feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/QuizSolveTimeSlider.kt
+++ feature/quiz/src/main/java/kr/boostcamp_2024/course/quiz/component/QuizSolveTimeSlider.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,7 +49,8 @@ fun QuizSolveTimeSlider(
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, locale = "ko")
+@PreviewLightDark
 @Composable
 fun QuizSolveTimeSliderPreview() {
     QuizSolveTimeSlider(
