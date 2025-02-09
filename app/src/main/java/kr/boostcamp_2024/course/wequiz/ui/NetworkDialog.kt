a12fe2792704d734775aa0d1b9a9588c06d74309
mk43313@gmail.com <mk43313@gmail.com>
2025-02-09 14:46:17 +0900

    feat: NetworkDialog 생성(#208)

---
+++ app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/NetworkDialog.kt
@@ -0,0 +1,67 @@
+package kr.boostcamp_2024.course.wequiz.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kr.boostcamp_2024.course.data.network.NetworkState
+import kr.boostcamp_2024.course.designsystem.ui.annotation.PreviewKoLightDark
+import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
+import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizBaseDialog
+import kr.boostcamp_2024.course.quiz.R
+
+@Composable
+fun NetworkDialog(
+    networkState: NetworkState,
+) {
+    var showDialog by remember { mutableStateOf(false) }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(networkState) {
+        if (networkState is NetworkState.NotConnected) {
+            showDialog = true
+        } else {
+            showDialog = false
+        }
+    }
+    if (showDialog) {
+        WeQuizBaseDialog(
+            title = "이용중인 네트워크 환경이 불안정합니다. 무선통신(5G/LTE 또는 Wi-Fi)상태를 확인 후 다시 이용 부탁드립니다.",
+            dialogImage = painterResource(id = R.drawable.quiz_create_ai_profile),
+            confirmTitle = "재시도",
+            dismissTitle = "닫기",
+            onConfirm = {
+                coroutineScope.launch {
+                    showDialog = false
+                    delay(500L)
+                    showDialog = networkState == NetworkState.NotConnected
+                }
+            },
+            onDismissRequest = {
+                showDialog = false
+            },
+            content = { },
+        )
+    }
+}
+
+@PreviewKoLightDark
+@Composable
+private fun NetworkDialogPreview() {
+    WeQuizTheme {
+        WeQuizBaseDialog(
+            title = "이용중인 네트워크 환경이 불안정합니다. 무선통신(5G/LTE 또는 Wi-Fi)상태를 확인 후 다시 이용 부탁드립니다.",
+            dialogImage = painterResource(id = R.drawable.quiz_create_ai_profile),
+            confirmTitle = "재시도",
+            onConfirm = {},
+            onDismissRequest = { },
+            content = { },
+        )
+    }
+}
