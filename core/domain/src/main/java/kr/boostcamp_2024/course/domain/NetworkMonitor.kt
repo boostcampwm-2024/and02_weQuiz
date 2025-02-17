0ca4f64435e147d1901d1387536a027056d10f37
pengcon <vpdzjs@gmail.com>
2025-02-17 18:16:46 +0900

    feat: NetworkMonitor Interface 추가  (#208)

---
+++ core/domain/src/main/java/kr/boostcamp_2024/course/domain/NetworkMonitor.kt
@@ -0,0 +1,8 @@
+package kr.boostcamp_2024.course.domain
+
+import kotlinx.coroutines.flow.StateFlow
+
+interface NetworkMonitor {
+    val networkState: StateFlow<NetworkState>
+    fun checkCurrentNetworkState()
+}
