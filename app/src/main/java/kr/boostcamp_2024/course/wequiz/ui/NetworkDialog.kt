69f10de3a4b715840459f48837e02827ae53110e
pengcon <vpdzjs@gmail.com>
2025-03-03 21:31:04 +0900

    refactor: networkState Boolean으로 변경, String 추출 (#208)

--- app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/NetworkDialog.kt
+++ app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/NetworkDialog.kt
@@ -8,35 +8,35 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kr.boostcamp_2024.course.designsystem.ui.annotation.PreviewKoLightDark
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.designsystem.ui.theme.component.WeQuizBaseDialog
-import kr.boostcamp_2024.course.domain.NetworkState
 import kr.boostcamp_2024.course.quiz.R
 
 @Composable
-fun NetworkDialog(
-    networkState: NetworkState,
+internal fun NetworkDialog(
+    networkState: Boolean,
 ) {
     var showDialog by remember { mutableStateOf(false) }
     val coroutineScope = rememberCoroutineScope()
 
     LaunchedEffect(networkState) {
-        showDialog = networkState is NetworkState.NotConnected
+        showDialog = !networkState
     }
     if (showDialog) {
         WeQuizBaseDialog(
-            title = "이용중인 네트워크 환경이 불안정합니다. 무선통신(5G/LTE 또는 Wi-Fi)상태를 확인 후 다시 이용 부탁드립니다.",
+            title = stringResource(kr.boostcamp_2024.course.wequiz.R.string.network_dialog_title),
             dialogImage = painterResource(id = R.drawable.quiz_create_ai_profile),
-            confirmTitle = "재시도",
+            confirmTitle = stringResource(kr.boostcamp_2024.course.wequiz.R.string.network_dialog_confirm_title),
             dismissTitle = null,
             onConfirm = {
                 coroutineScope.launch {
                     showDialog = false
                     delay(500L)
-                    showDialog = (networkState == NetworkState.NotConnected)
+                    showDialog = (!networkState)
                 }
             },
             onDismissRequest = {
