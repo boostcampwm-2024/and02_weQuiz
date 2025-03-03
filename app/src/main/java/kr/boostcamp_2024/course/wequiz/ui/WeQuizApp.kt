2f43be552202c08afe655c68650199f527a65da9
pengcon <vpdzjs@gmail.com>
2025-03-03 21:34:18 +0900

    refactor: collectAsStateWithLifecycle로 변경 (#208)

--- app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/WeQuizApp.kt
+++ app/src/main/java/kr/boostcamp_2024/course/wequiz/ui/WeQuizApp.kt
@@ -3,12 +3,12 @@ package kr.boostcamp_2024.course.wequiz.ui
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import kotlinx.coroutines.launch
 import kr.boostcamp_2024.course.designsystem.ui.theme.WeQuizTheme
 import kr.boostcamp_2024.course.domain.NetworkMonitor
@@ -19,7 +19,7 @@ fun WeQuizApp(networkMonitor: NetworkMonitor) {
     val snackbarHostState = remember { SnackbarHostState() }
     val coroutineScope = rememberCoroutineScope()
     val localContextResource = LocalContext.current.resources
-    val networkState by networkMonitor.networkState.collectAsState()
+    val networkState by networkMonitor.networkState.collectAsStateWithLifecycle(true)
     val onShowErrorSnackbar: (throwable: Throwable) -> Unit = { throwable ->
         coroutineScope.launch {
             snackbarHostState.showSnackbar(
