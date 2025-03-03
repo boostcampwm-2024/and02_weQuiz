0cc3bdda608076202ebebc5900091d43cb3820fb
pengcon <vpdzjs@gmail.com>
2025-03-03 21:29:34 +0900

    feat: targetSdk 35로 변경 (#208)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ android {
 
     defaultConfig {
         applicationId = "kr.boostcamp_2024.course.wequiz"
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 6
         versionName = "1.2.1"
 
